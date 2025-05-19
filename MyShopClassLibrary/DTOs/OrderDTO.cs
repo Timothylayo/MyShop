aa85ddd2e4c0c0beccfa682fd08f7f6cb2302d16
Timothy Omolayole <timothylayo27@gmail.com>
2025-05-19 05:50:47 +0100

    Updates

--- MyShopClassLibrary/DTOs/OrderDTO.cs
+++ MyShopClassLibrary/DTOs/OrderDTO.cs
@@ -4,14 +4,14 @@ namespace MyShopClassLibrary.DTOs;
 
 public class OrderDTO
 {
-    public string UserId { get; set; }
+    public string? UserId { get; set; }
     public string? OrderId { get; set; }
 
     [MaxLength(100)]
     public string? Address { get; set; }
     [Phone]
     public string? PhoneNumber { get; set; }
-    public string CreatedDate { get; set; }
+    public string? CreatedDate { get; set; }
     public decimal TotalPrice { get; set; }
     public string? ShippingStatus { get; set; }
 }
